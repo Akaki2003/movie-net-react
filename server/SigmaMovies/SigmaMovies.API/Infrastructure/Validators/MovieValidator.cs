5e6723e8a66789fcc045809c6ff0a91e5209d623
akaki ujarashvili <akaki.ujarashvili.1@btu.edu.ge>
2023-06-04 12:53:47 +0400

    added validations for password and ratings and uhm I dont remember

--- server/SigmaMovies/SigmaMovies.API/Infrastructure/Validators/MovieValidator.cs
+++ server/SigmaMovies/SigmaMovies.API/Infrastructure/Validators/MovieValidator.cs
@@ -24,6 +24,32 @@ namespace SigmaMovies.API.Infrastructure.Validators
                 .NotEmpty();
             RuleFor(x => x.Img)
                 .NotEmpty().MaximumLength(500);
+            RuleFor(x => x.Rating.IMDb).InclusiveBetween(1, 10);
+            RuleFor(x => x.Rating.RottenTomatoes).InclusiveBetween(0, 100);
+        }
+    }
+    public class MoviePutValidator : AbstractValidator<MovieRequestPutModel>
+    {
+        public MoviePutValidator()
+        {
+            RuleFor(x => x.Title)
+                    .NotEmpty().WithMessage("Movie's title must not be empty")
+                    .MaximumLength(300).WithMessage("title's length exceeded the limit");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Movie's Description must not be empty")
+                .MaximumLength(1000).WithMessage("Title's length exceeded the limit");
+
+            RuleFor(x => x.Color)
+                .NotEmpty().MaximumLength(8);
+            RuleFor(x => x.Color2)
+                .NotEmpty().MaximumLength(8);
+            RuleFor(x => x.Video)
+                .NotEmpty();
+            RuleFor(x => x.Img)
+                .NotEmpty().MaximumLength(500);
+            RuleFor(x => x.Rating.IMDb).InclusiveBetween(1, 10);
+            RuleFor(x => x.Rating.RottenTomatoes).InclusiveBetween(0, 100);
         }
     }
 }
---
+++ server/SigmaMovies/SigmaMovies.API/Infrastructure/Validators/UserValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SigmaMovies.Application.Users.Requests;
+
+namespace SigmaMovies.API.Infrastructure.Validators
+{
+    public class UserValidator : AbstractValidator<UserRequestModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Username).NotEmpty().EmailAddress();
+            RuleFor(x=>x.Password).NotEmpty().Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$");
+        }
+    }
+}
